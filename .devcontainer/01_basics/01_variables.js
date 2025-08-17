81ab71e8a8f99e168b8129907c4a2d0e8b42faec
satt698 <samiisattar@gmail.com>
2025-08-17 10:49:06 +0000

    conversions

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+// const accountId = 1234
+// let accountEmail = "jan@gmail.com"
+// var accountPassword = "54321"
+// let accountCity = "Kabul"
+
+// //accounId = 2 // not allowed//
+// //accountCity = "Paktia"
+// console.table ([accountId, accountEmail, accountPassword,accountCity])
+
+const accountId = 2233
+let accountEmail = "sami@gmail.com"
+var accountPassword = "123456"
+let accountCity = "kabul"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = 33
+
+// console.log (typeof score)
+// console.log (typeof (score))
+
+
+// let score = "56"
+
+// let valueInNumber = Number(score)
+// console.log(score)
+// console.log (typeof score)
+// console.log (typeof (score))
+// console.log (typeof valueInNumber)
+
+//let score = "Sami"
+//let valueInNumber = Number (score)
+//console.log (score)
+//console.log  (typeof score)
+//console.log (typeof (score))
+//console.log (typeof valueInNumber)
+//console.log (valueInNumber)
+// "33" => 33
+// "33abc" => NaN
+// Null => 0  and undifind => NaN
+// true => 1 and false => 0
+let isloggedIN = "sami"
+let boolianIsLoggedIn = Boolean (isloggedIN)
+console.log(isloggedIN)
+
+
+
+
+
+
