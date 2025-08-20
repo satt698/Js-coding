c936b39f4dafb57705f622216197c4608618c1b4
satt698 <samiisattar@gmail.com>
2025-08-20 04:47:54 +0000

    conversionOperation

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -23,12 +23,31 @@
 // "33abc" => NaN
 // Null => 0  and undifind => NaN
 // true => 1 and false => 0
-let isloggedIN = "sami"
-let boolianIsLoggedIn = Boolean (isloggedIN)
-console.log(isloggedIN)
+//let isloggedIN = "sami"
+//let boolianIsLoggedIn = Boolean (isloggedIN)
+//console.log(isloggedIN)
+//************************** Operations ***********************/
 
+// let value = 3
+// let negValue = -value
+// console.log (negValue)
 
+// let str1 = "sami"
+// let str2 = " Khan"
+// let str3 = " zazai"
 
+// let str4 = str1 + str2 + str3
+// console.log (str4 )
 
+// let string1 = "Abdul"
+// let string2 = " Musavir"
+// let string3 = " Sattar"
+// let string4 = string1 + string2 +string3
+// console.log (string4)
+
+let num1 = num2 = num3 = 2+2
+let gameCounter =100
+gameCounter++
+console.log(gameCounter)
 
 
